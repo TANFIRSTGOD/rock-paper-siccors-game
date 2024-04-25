eaee8fa06adca4e060f85b5b26965cea3aa057ea
tan05 <tan.tantham.tan@gmail.com>
2024-04-26 00:25:27 +0700

    window

---
+++ main.py
@@ -0,0 +1,12 @@
+#rock paper sciccors game
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Rock Paper Sciccors")
+
+def handle_button_press(event):
+    window.destroy()
+
+# Start the event loop.
+window.mainloop()

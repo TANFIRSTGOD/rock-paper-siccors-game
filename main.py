6c89a9fb3da328e86e25d1ef5e1081fcad9700d7
tan05 <tan.tantham.tan@gmail.com>
2024-04-26 00:28:51 +0700

    window resize

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Rock Paper Sciccors")
+window.minsize(width=250, height=250)
 
 def handle_button_press(event):
     window.destroy()
+
+title = tk.Label(window, text="Rock Paper Sciccors!")
+title.pack()
 
 # Start the event loop.
 window.mainloop()
